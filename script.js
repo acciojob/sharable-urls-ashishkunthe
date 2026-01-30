77a0d7a3edf4fe6ee2d7dbb44170b8bb5e66123a
Ashish Kunthe <160906505+ashishkunthe@users.noreply.github.com>
2026-01-30 22:58:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // your code here
+function submit(){
+	const name=document.querySelector("#name").value
+	const year=document.querySelector("#year").value
+
+	if(name && year){
+		document.querySelector("#url").textContent =`https://localhost:8080/?name=${name}&year=${year}`
+	}
+ else if(name){
+	 document.querySelector("#url").textContent =`https://localhost:8080/?name=${name}`
+ }
+else if(year){
+	document.querySelector("#url").textContent = `https://localhost:8080/?year=${year}`
+}
+else document.querySelector("#url").textContent ="https://localhost:8080/"
+}
